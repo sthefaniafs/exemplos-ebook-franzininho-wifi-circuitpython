1acf6d992be56c17ba5032bad47f6591a00733fb
sthefaniafs <sthefaniafernandes03@gmail.com>
2024-10-28 10:57:29 -0300

    novos exemplos

---
+++ 11 - Sensor de temperatura e umidade DHT11/sensor_dht11.py
@@ -0,0 +1,20 @@
+""" Leitura de temperatura e umidade com DHT11 """
+
+import board
+import time
+import adafruit_dht
+
+dht = adafruit_dht.DHT11(board.IO2)
+
+while True:
+   try:
+       temperatura = dht.temperature
+       umidade = dht.humidity
+       # Imprime valores lidos na serial
+       print("Temperatura: {:.1f} °C \t Umidade: {}%".format(temperatura, umidade))
+   except RuntimeError as e:
+       # A leitura do DHT11 pode falhar
+       print("Falha na leitura do DHT11: ", e.args)
+
+   time.sleep(1)
+
---
+++ 8 - Franzininho Wi-Fi com CircuitPython – Dicas e Truques/tamanho_disco.py
@@ -0,0 +1,7 @@
+import os
+import gc
+
+fs_stat = os.statvfs('/')
+print("Disk size in MB", fs_stat[0] * fs_stat[2] / 1024 / 1024)
+print("Free space in MB", fs_stat[0] * fs_stat[3] / 1024 / 1024)
+print( gc.mem_free() )
